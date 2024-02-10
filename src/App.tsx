0b41e18afb80cfaaa4cc14ced3741df5cfab4380
unhaten <csgold2002@mail.ru>
2024-02-10 22:35:53 +0300

    testApp v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import List from "@mui/material/List"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemText from "@mui/material/ListItemText"
+import Collapse from "@mui/material/Collapse"
+import ExpandLess from "@mui/icons-material/ExpandLess"
+import ExpandMore from "@mui/icons-material/ExpandMore"
+import { Box, Button, ListItem } from "@mui/material"
+import "./App.css"
+import { useEffect, useState } from "react"
+import IService from "./types/IService"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [selectedIndex, setSelectedIndex] = useState<null | number>(null)
+	const [data, setData] = useState<null>(null)
+	const [refetch, setRefetch] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleClick = (index: number) => {
+		if (selectedIndex === index) {
+			setSelectedIndex(null)
+		} else {
+			setSelectedIndex(index)
+		}
+	}
+
+	useEffect(() => {
+		fetch("http://localhost:3000/services")
+			.then(res => res.json())
+			.then(data => setData(data))
+	}, [refetch])
+
+	// console.log(data)
+
+	return (
+		<Box>
+			<Button
+				variant="contained"
+				onClick={() => setRefetch(prev => !prev)}
+			>
+				<span>refetch</span>
+			</Button>
+
+			<List
+				sx={{
+					width: "100%",
+					maxWidth: 420,
+					minWidth: 250,
+					bgcolor: "#181818",
+					marginTop: 2
+				}}
+				component="nav"
+			>
+				{data &&
+					data.map((item: IService) => {
+						console.log(item)
+						return (
+							<>
+								<ListItem key={item.id}>
+									<ListItemButton
+										onClick={() =>
+											item.node === 1 &&
+											handleClick(item.id)
+										}
+									>
+										<ListItemText
+											sx={{
+												"& span": {
+													fontWeight:
+														item.node === 1
+															? 600
+															: 400
+												}
+											}}
+											primary={
+												`${item.name}` +
+												`${
+													item.node === 0
+														? ` - ${item.price}`
+														: ""
+												}`
+											}
+										/>
+										{item.node === 1 ? (
+											item.id === selectedIndex ? (
+												<ExpandLess />
+											) : (
+												<ExpandMore />
+											)
+										) : (
+											""
+										)}
+									</ListItemButton>
+								</ListItem>
+								<Collapse
+									in={item.id === selectedIndex}
+									timeout="auto"
+									unmountOnExit
+								>
+									<List component="div" disablePadding>
+										<ListItemButton sx={{ ml: 4 }}>
+											<ListItemText primary="Starred" />
+										</ListItemButton>
+										{/* {data.forEach(elemnt => (
+											<Box>{elemnt.id}</Box>
+										))} */}
+									</List>
+								</Collapse>
+							</>
+						)
+					})}
+			</List>
+		</Box>
+	)
 }
-
-export default App
---
+++ src/types/IService.ts
@@ -0,0 +1,8 @@
+export default interface IService {
+	id: number
+	head: number
+	name: string
+	node: number
+	price: number
+	sorthead: number
+}
